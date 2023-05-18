f8a7120382fa26952e292bad6b9056ba55f488fb
ruzell <arturocrisanto1@gmail.com>
2023-05-18 21:11:42 +0800

    client

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState ,useEffect} from "react";
+import Axios from "axios"; 
 
 function App() {
+  const [listOfUsers, setListOfUsers] = useState([]);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [username,setUsername] = useState("");
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/getUsers")
+      .then((response) => {
+        setListOfUsers(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []); 
+
+  const createUser = (user) => {
+    Axios.post("http://localhost:3001/createUser", {
+      name,
+      age,
+      username
+    })
+    .then((response) => {
+      setListOfUsers([...listOfUsers, {
+      name,
+      age,
+      username
+      }])
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Age</th>
+            <th scope="col">Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listOfUsers.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.age}</td>
+              <td>{user.username}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+  
+      <div>
+        <h2>Add User</h2>
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" 
+          onChange={(event)=>{
+            setName(
+            event.target.value
+            )}} />
+        <label htmlFor="age">Age:</label>
+        <input type="number" id="age" 
+        onChange={(event)=>{
+          setAge(
+            event.target.value
+            )}}/>
+        <label htmlFor="username">Username:</label>
+        <input type="text" id="username" 
+        onChange={(event)=>{
+          setUsername(
+            event.target.value
+            )}}/>
+        <button onClick={createUser}>Add</button>
+      </div>
     </div>
   );
+  
 }
+
 
 export default App;
